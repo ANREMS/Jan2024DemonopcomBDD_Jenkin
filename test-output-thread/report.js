5ef7e00b9b31479026861311d444511f39d3e340
Annadurai <choannadurai@gmail.com>
2024-06-29 10:47:43 +0100

    Initial code commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"a16beb54-87e4-476b-9501-7dcec7df12ff","feature":"Login","scenario":"Login","start":1719654246562,"group":1,"content":"","tags":"@alogin,","end":1719654258663,"className":"passed"},{"id":"ba52b845-1c0a-46e3-b955-46046b771d63","feature":"Registration","scenario":"Registration","start":1719654220184,"group":1,"content":"","tags":"@bregistration,","end":1719654246548,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+});
