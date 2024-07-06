636878eb0516e4d419e4f6fb6aaf29feeedb2f09
Annadurai <choannadurai@gmail.com>
2024-07-06 15:00:32 +0100

    Additional files updated

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"1ffef096-8083-4743-ab68-92b942c50241","feature":"Registration","scenario":"Registration","start":1719919738759,"group":1,"content":"","tags":"@bregistration,","end":1719919758598,"className":"failed"},{"id":"5f77e5d0-7285-48d8-8b3b-ffec0d20320e","feature":"Login","scenario":"Login","start":1719919758622,"group":1,"content":"","tags":"@alogin,","end":1719919768661,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"71dc11e1-93e7-4a97-b104-89c2bbcc9597","feature":"Login","scenario":"Login","start":1720248947736,"group":1,"content":"","tags":"@alogin,","end":1720249014512,"className":"passed"},{"id":"45666d6c-79ce-4724-ba5a-42dd053b3c4a","feature":"Registration","scenario":"Registration","start":1720248828150,"group":1,"content":"","tags":"@bregistration,","end":1720248947717,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
 });
